1ef374e4cfcb29cf922328f05144ad04bd7b96bc
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2024-05-30 20:44:25 +0100

    css changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
-  userName: string = "Otunba"
+  userName: string = "Otunba Aina"
   currentDate: string = "";
 
   ngOnInit(): void {
