ca282d8022090b51a73a3883c361ea754e6bd286
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2024-05-30 19:30:04 +0100

    header component finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DeletedComponent } from './components/deleted/deleted.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideNavComponent,
-    HeaderComponent
+    HeaderComponent,
+    DashboardComponent,
+    DeletedComponent
   ],
   imports: [
     BrowserModule,
