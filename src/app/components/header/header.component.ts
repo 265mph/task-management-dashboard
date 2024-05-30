1d9a4d66af50bf719dc439a9ad3e96aad87d7f9a
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2024-05-30 18:00:27 +0100

    added header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  userName: string = "Otunba"
+  currentDate: string = "";
 
+  ngOnInit(): void {
+    this.showDate()
+  }
+  
+  showDate() {
+    const date = new Date();
+    this.currentDate = date.toDateString()
+  }
 }
